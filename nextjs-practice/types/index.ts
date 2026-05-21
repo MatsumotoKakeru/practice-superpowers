1c307281722809a84505019f4745ce6b7235494f
MatsumotoKakeru <kakeru.matsumoto@justenough.jp>
2026-05-21 16:24:51 +0900

    feat: Snippet型定義を追加

---
+++ nextjs-practice/types/index.ts
@@ -0,0 +1,18 @@
+export type Snippet = {
+  id: number
+  url: string
+  title: string
+  code: string
+  language: string
+  style: string
+  linenos: boolean
+  owner: string | null
+  highlight: string
+}
+
+export type PaginatedResponse<T> = {
+  count: number
+  next: string | null
+  previous: string | null
+  results: T[]
+}
